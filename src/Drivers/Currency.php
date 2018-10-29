d760fb4ad1676f6528f55a3353eb5fcb40bb7f9a
kgregorywd <kgregorywd@gmail.com>
2018-10-29 19:49:21 +0500

    Вывод основной стоимости в евро если нету данных

--- src/Drivers/Currency.php
+++ src/Drivers/Currency.php
@@ -48,8 +48,13 @@ class Currency
         $alias = $locale === 'tr' ? 'lira' : 'euro';
         $currency = $object->currencies->where('alias', $alias)->first();
 
-        $price = optional($currency->pivot)->price;
+        if ($currency) {
 
-        return "{$price} {$currency->icon}";
+            $price = optional($currency->pivot)->price;
+
+            return "{$price} {$currency->icon}";
+        } else {
+            return "{$object->price} €";
+        }
     }
 }
